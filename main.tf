2d09c44a0406e88c8d5a20b49e05910df11fb9d7
Asutosh Ghanto <asutosh@clevertap.com>
2024-08-30 23:38:56 +0530

    vpc: add Nat gateway and write custom kubernets module for POC.

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ module "vpc" {
   private_subnets = ["10.12.1.0/24", "10.12.2.0/24", "10.12.3.0/24"]
   public_subnets  = ["10.12.4.0/24", "10.12.5.0/24", "10.12.6.0/24"]
 
-  enable_nat_gateway = false
-  single_nat_gateway  = false
+  enable_nat_gateway = true
+  single_nat_gateway  = true
   enable_vpn_gateway = false
 
   tags = {
@@ -17,63 +17,3 @@ module "vpc" {
     Environment = "dev"
   }
 }
-
-#module "eks" {
-#  source  = "terraform-aws-modules/eks/aws"
-#  version = "~> 20.0"
-#
-#  cluster_name    = "kubernets-cluster"
-#  cluster_version = "1.30"
-#
-#  cluster_endpoint_public_access  = true
-#
-#  cluster_addons = {
-#    coredns = {
-#      most_recent = true
-#    }
-#    kube-proxy = {
-#      most_recent = true
-#    }
-#    vpc-cni = {
-#      most_recent = true
-#    }
-#  }
-#  vpc_id                   = module.vpc.vpc_id
-#  subnet_ids               = module.vpc.private_subnets
-#  control_plane_subnet_ids = module.vpc.public_subnets
-#
-#  eks_managed_node_group_defaults = {
-#    ami_type       = "AL2_x86_64"
-#    instance_types = ["m5.large"]
-#
-#    attach_cluster_primary_security_group = true
-#  }
-#
-#  eks_managed_node_groups = {
-#    nginx-cluster-wg = {
-#      min_size     = 0
-#      max_size     = 2
-#      desired_size = 0
-#
-#      instance_types = ["t3.large"]
-#      capacity_type  = "SPOT"
-#
-#      tags = {
-#        ExtraTag = "helloworld"
-#      }
-#    }
-#  }
-#
-#  # Cluster access entry
-#  # To add the current caller identity as an administrator
-#  enable_cluster_creator_admin_permissions = true
-#
-#  # cloudwatch retention days
-#  cloudwatch_log_group_retention_in_days=3
-#
-#
-#  tags = {
-#    Environment = "dev"
-#    Terraform   = "true"
-#  }
-#}
