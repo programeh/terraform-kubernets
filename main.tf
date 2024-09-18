a68388a36581c5a58933c282696b81dd5eced7fe
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2024-09-18 13:05:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.12.0.0/16"
-
-  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  private_subnets = ["10.12.1.0/24", "10.12.2.0/24", "10.12.3.0/24"]
-  public_subnets  = ["10.12.4.0/24", "10.12.5.0/24", "10.12.6.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway  = true
-  enable_vpn_gateway = false
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
+#module "vpc" {
+#  source = "terraform-aws-modules/vpc/aws"
+#
+#  name = "my-vpc"
+#  cidr = "10.12.0.0/16"
+#
+#  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+#  private_subnets = ["10.12.1.0/24", "10.12.2.0/24", "10.12.3.0/24"]
+#  public_subnets  = ["10.12.4.0/24", "10.12.5.0/24", "10.12.6.0/24"]
+#
+#  enable_nat_gateway = true
+#  single_nat_gateway  = true
+#  enable_vpn_gateway = false
+#
+#  tags = {
+#    Terraform = "true"
+#    Environment = "dev"
+#  }
+#}
